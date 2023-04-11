d2105fc62111c1c0f0a62085b83f2ef295f9c36d
bespalov0928 <bespalov0928@mail.ru>
2023-04-11 09:16:18 +0700

    work_car_accident_V2: Подключение jacoco, lombok, Checkstyle

--- src/main/java/ru/work/work_car_accident_V2/Main.java
+++ src/main/java/ru/work/work_car_accident_V2/Main.java
@@ -2,10 +2,18 @@ package ru.work.work_car_accident_V2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        System.out.println("Go to http://localhost:8080/index");
     }
 }
