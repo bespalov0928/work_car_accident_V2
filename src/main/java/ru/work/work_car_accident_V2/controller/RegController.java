93cecb5e15b0dba9e0395e1ee531bf10b77454a6
bespalov0928 <bespalov0928@mail.ru>
2023-04-23 20:17:29 +0700

    work_car_accident_V2: Добавил регистрацию

---
+++ src/main/java/ru/work/work_car_accident_V2/controller/RegController.java
@@ -0,0 +1,38 @@
+package ru.work.work_car_accident_V2.controller;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.work.work_car_accident_V2.model.Role;
+import ru.work.work_car_accident_V2.model.Status;
+import ru.work.work_car_accident_V2.model.User;
+import ru.work.work_car_accident_V2.repository.JpaUserRepository;
+import ru.work.work_car_accident_V2.service.SimpleUserService;
+
+@Controller
+public class RegController {
+    private final PasswordEncoder encoder;
+    private final SimpleUserService userService;
+
+    public RegController(PasswordEncoder encoder, SimpleUserService userService) {
+        this.encoder = encoder;
+        this.userService = userService;
+    }
+
+    @PostMapping("/reg")
+    public String regSave(@ModelAttribute User user) {
+        user.setStatus(Status.ACTIVE);
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole(Role.ADMIN);
+        userService.save(user);
+        return "redirect:/auth/login";
+    }
+
+    @GetMapping("/reg")
+    public String regPage() {
+        return "reg";
+    }
+}
